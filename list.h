0bc5ee6f30f2fce0f8612f640fb120c60534dc0a
Chen <yuc185@ieng6-201.ucsd.edu>
2025-07-23 11:25:53 -0700

    hi

--- list.h
+++ list.h
@@ -20,4 +20,34 @@ int8_t insert(List* list, UStr s, int32_t index);
 int8_t listRemoveAt(List* list, int32_t index);
 List split(UStr s, UStr separator);
 
+//helper
+void listAppend(List* list, UStr element) {
+    if (list->size == list->capacity) {
+        list->capacity *= 2;
+        UStr* newItems = realloc(list->data, list->capacity * sizeof(UStr));
+        if (newItems == NULL) {
+            return;
+        }
+        list->data = newItems;
+    }
+    list->data[list->size++] = element;
+}
+
+UStr substr(UStr s, int32_t start, int32_t end) {
+    UStr result;
+    result.bytes = end - start;
+
+    result.contents = malloc(result.bytes);
+    if (!result.contents) {
+        result.bytes = 0;
+        return result;
+    }
+
+    for (int32_t i = 0; i < result.bytes; i++) {
+        result.contents[i] = s.contents[start + i];
+    }
+
+    return result;
+}
+
 #endif
